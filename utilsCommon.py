c5ddd523cce1f6c3513bac61596c4163712f0622
andrebeu <aobeukers@gmail.com>
2022-12-08 10:13:00 -0300

    snapshot

--- utilsCommon.py
+++ utilsCommon.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 PMATCH, PSLURE, PCLURE, pr_nomatch = 0.4, 0.2, 0.2, 0.2
 
 # exp params (imported)
-nbackL = [2,3]
+nbackL = [2, 3]
 EXPLEN = 48
 NSTIM = 8
 # context params
@@ -26,8 +26,9 @@ hiddim = SDIM * 4
 
 ## Context drift
 
+
 def spherical_drift(n_steps=CSTEPS, dim=CDIM, var=CVAR, mean=CMEAN):
-    """ 
+    """
     # model of CR as spherical coordinates updated by a noisy drift
     """
 
@@ -48,77 +49,84 @@ def spherical_drift(n_steps=CSTEPS, dim=CDIM, var=CVAR, mean=CMEAN):
     slen = n_steps
     ctxt = np.zeros((slen, dim))
     for i in range(slen):
-        noise = np.random.normal(mean, var, size=(dim - 1)) # add a separately-drawn Gaussian to each spherical coord
+        noise = np.random.normal(
+            mean, var, size=(dim - 1)
+        )  # add a separately-drawn Gaussian to each spherical coord
         ros += noise
         ctxt[i] = convert_spherical_to_angular(dim, ros)
     return ctxt
 
+
 # ctxt_fn = lambda n_steps: spherical_drift(n_steps=n_steps)
 
 
-## plot and eval funs 
+## plot and eval funs
 
-def plot_train_accuracy(score_tr,ttype,ssizeL,figure_path=""):
-  task_labels = ["setsize " + str(ssize) for ssize in ssizeL]
-  colors = [["#edc174", "#d4982f", "#c27e08", "#a1690a"], ["#8dcbf7", "#61b3ed", "#2a86c7", "#0762a3"]]
-  labels = ["match", "slure", "clure", "nomatch"]
-  n_intervals = 1000
-  for i in range(len(score_tr)):
-    task_score = score_tr[i]
-    task_color = colors[i]
-    task_trialtypes = ttype[i]
-    for tt in range(4):
-      filt_inds = task_trialtypes == tt
-      ep_ttype = np.extract(filt_inds, task_score)
-      ep_ttype = ep_ttype[:-(len(ep_ttype)%n_intervals)]
-      ac = ep_ttype.reshape(-1, n_intervals).mean(1)
-      lt = task_labels[i] + " " + labels[tt]
-      plt.plot(ac, color=task_color[tt], label=lt)
-  plt.legend(loc="best")
-  plt.ylim(0, 1)
-  plt.ylabel('Train accuracy')
-  plt.savefig(figure_path + "/train-accuracy")
-  plt.close('all')
+
+def plot_train_accuracy(score_tr, ttype, ssizeL, figure_path=""):
+    task_labels = ["setsize " + str(ssize) for ssize in ssizeL]
+    colors = [
+        ["#edc174", "#d4982f", "#c27e08", "#a1690a"],
+        ["#8dcbf7", "#61b3ed", "#2a86c7", "#0762a3"],
+    ]
+    labels = ["match", "slure", "clure", "nomatch"]
+    n_intervals = 1000
+    for i in range(len(score_tr)):
+        task_score = score_tr[i]
+        task_color = colors[i]
+        task_trialtypes = ttype[i]
+        for tt in range(4):
+            filt_inds = task_trialtypes == tt
+            ep_ttype = np.extract(filt_inds, task_score)
+            ep_ttype = ep_ttype[: -(len(ep_ttype) % n_intervals)]
+            ac = ep_ttype.reshape(-1, n_intervals).mean(1)
+            lt = task_labels[i] + " " + labels[tt]
+            plt.plot(ac, color=task_color[tt], label=lt)
+    plt.legend(loc="best")
+    plt.ylim(0, 1)
+    plt.ylabel("Train accuracy")
+    plt.savefig(figure_path + "/train-accuracy")
+    plt.close("all")
+
 
 def eval_by_ttype(net, sample_fn, taskintL, ssizeL, neps):
-  """ 
-  eval on given task for separate trial types
-  returns evac on (match,nomatch,slure,clure)
-  """
+    """
+    eval on given task for separate trial types
+    returns evac on (match,nomatch,slure,clure)
+    """
 
-  taskL_ev = []
-  # generate a list of tasks which are trials all of one kind so we can see accuracy by trial type
-  for task_int in taskintL:
-    taskL_ev.append([task_int, sample_fn(1, 0, 0), ssizeL[task_int]])
-    taskL_ev.append([task_int, sample_fn(0, 0, 0), ssizeL[task_int]])
-    taskL_ev.append([task_int, sample_fn(0, 1, 0), ssizeL[task_int]])
-    taskL_ev.append([task_int, sample_fn(0, 0, 1), ssizeL[task_int]])
+    taskL_ev = []
+    # generate a list of tasks which are trials all of one kind so we can see accuracy by trial type
+    for task_int in taskintL:
+        taskL_ev.append([task_int, sample_fn(1, 0, 0), ssizeL[task_int]])
+        taskL_ev.append([task_int, sample_fn(0, 0, 0), ssizeL[task_int]])
+        taskL_ev.append([task_int, sample_fn(0, 1, 0), ssizeL[task_int]])
+        taskL_ev.append([task_int, sample_fn(0, 0, 1), ssizeL[task_int]])
 
-  evsc, ttype = run_model_for_epochs(
-    net, 
-    taskL_ev,
-    training=False,
-    neps_per_task=neps,
-    verb=False
-  )
+    evsc, ttype = run_model_for_epochs(
+        net, taskL_ev, training=False, neps_per_task=neps, verb=False
+    )
 
-  evac = evsc.mean(1)
-  print(evac)
-  # regroup the list of scores into a list of lists grouped by the setsize
-  scores_by_ss = [[]] * len(taskintL)
-  for task_int in taskintL:
-    scores_by_ss[task_int] = evac[task_int*4:(task_int+1)*4]
-  return scores_by_ss
-
-def plot_accuracy_by_trial_type(evac,taskintL,ssizeL,figure_path=""):
-  for tidx, (task_int, ssize) in enumerate(zip(taskintL, ssizeL)):
-    plt.title('Accuracy by trial type')
-    plt.bar(np.arange(4) + (.45 * tidx), evac[tidx], width=.45, label="setsize:" + str(ssize))
-  plt.legend()
-  plt.xticks(range(4), ['match', 'nomatch', 'slure', 'clure'])
-  if not False:
-    plt.savefig(figure_path + "/trial-type-accuracy")
-    plt.close('all')
+    evac = evsc.mean(1)
+    print(evac)
+    # regroup the list of scores into a list of lists grouped by the setsize
+    scores_by_ss = [[]] * len(taskintL)
+    for task_int in taskintL:
+        scores_by_ss[task_int] = evac[task_int * 4 : (task_int + 1) * 4]
+    return scores_by_ss
 
 
-    
+def plot_accuracy_by_trial_type(evac, taskintL, ssizeL, figure_path=""):
+    for tidx, (task_int, ssize) in enumerate(zip(taskintL, ssizeL)):
+        plt.title("Accuracy by trial type")
+        plt.bar(
+            np.arange(4) + (0.45 * tidx),
+            evac[tidx],
+            width=0.45,
+            label="setsize:" + str(ssize),
+        )
+    plt.legend()
+    plt.xticks(range(4), ["match", "nomatch", "slure", "clure"])
+    if not False:
+        plt.savefig(figure_path + "/trial-type-accuracy")
+        plt.close("all")
